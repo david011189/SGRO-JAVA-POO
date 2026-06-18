614e599d38ed2ddc1d65dd2ae8a3b7b6fea6e8b9
JavierBedia <jbr151_9@hotmail.com>
2026-06-18 16:14:06 -0500

    frontend

--- src/gui/FrmReserva.java
+++ src/gui/FrmReserva.java
@@ -70,7 +70,7 @@ public class FrmReserva extends JFrame {
 	 */
 	public FrmReserva() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 650, 450);
+		setBounds(100, 100, 685, 460);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
